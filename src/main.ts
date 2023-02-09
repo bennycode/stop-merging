918b4f5d416a8f42336e264a5873d10f1b02cb07
Benny Neugebauer <mail@bennycode.com>
2023-02-09 23:44:40 +0100

    fix: Check only the latest status

--- src/main.ts
+++ src/main.ts
@@ -2,60 +2,75 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import type {GitHub} from '@actions/github/lib/utils';
 import {retry} from './retry';
-import {CheckSuites, filterCheckSuites} from './filterCheckSuites';
+import {AbortError} from './AbortError';
+import type {CheckSuite, CheckSuites} from './filterChecks';
 
-async function fetchCompletedSuites(
-  octokit: InstanceType<typeof GitHub>,
-  {
-    owner,
-    gitBranch,
-    repo,
-  }: {
-    gitBranch: string;
-    owner: string;
-    repo: string;
+type RepositoryInfo = {
+  gitBranch: string;
+  owner: string;
+  repo: string;
+};
+
+function assertChecks(
+  checkSuitesPayload: {
+    total_count: number;
+    check_suites: CheckSuites;
+  },
+  {repo, owner, gitBranch}: RepositoryInfo
+) {
+  const statusUpdates = checkSuitesPayload.check_suites.map(suite => suite.status);
+  console.log(
+    `Found "${checkSuitesPayload.total_count}" check suite(s) (${statusUpdates.join(
+      ', '
+    )}) for repository "${repo}" owned by "${owner}".`
+  );
+
+  if (checkSuitesPayload.total_count === 0) {
+    const errorMessage = `There are no required status checks for branch "${gitBranch}" of repository "${owner}/${repo}". Please have a look at: https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/collaborating-on-repositories-with-code-quality-features/about-status-checks`;
+    throw new AbortError(errorMessage);
   }
-): Promise<CheckSuites> {
+}
+
+/**
+ * Iterates over all check suites and resolves once the latest check suite is complete.
+ */
+async function fetchCompletedCheck(
+  octokit: InstanceType<typeof GitHub>,
+  {owner, gitBranch, repo}: RepositoryInfo
+): Promise<CheckSuite> {
   return new Promise(async (resolve, reject) => {
     // Checks API
     // https://docs.github.com/en/rest/checks/suites#list-check-suites-for-a-git-reference
-    const response = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}/check-suites', {
+    const {data: checkSuitesPayload} = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}/check-suites', {
       owner,
       ref: gitBranch,
       repo,
     });
 
-    const checkSuitesPayload = response.data;
+    assertChecks(checkSuitesPayload, {repo, owner, gitBranch});
 
-    const statusUpdates = checkSuitesPayload.check_suites.map(suite => suite.status);
+    const checks = checkSuitesPayload.check_suites;
+    const latestCheck = checks[checks.length - 1];
+    const isComplete = latestCheck.status === 'completed';
 
-    console.log(
-      `Found "${checkSuitesPayload.total_count}" check suite(s) (${statusUpdates.join(
-        ', '
-      )}) for repository "${repo}" owned by "${owner}".`
-    );
-
-    // Find pending check runs
-    const inProgress = filterCheckSuites(checkSuitesPayload.check_suites, gitBranch, 'in_progress');
-
-    if (inProgress.length > 0) {
+    if (isComplete) {
+      console.log(`Found completed check suite on branch "${gitBranch}".`);
+      resolve(latestCheck);
+    } else {
       const error = new Error('Waiting for check runs to finish...');
       reject(error);
-    } else {
-      const completed = filterCheckSuites(checkSuitesPayload.check_suites, gitBranch, 'completed');
-      console.log(`Found "${completed.length}" completed check suites on branch "${gitBranch}".`);
-      resolve(completed);
     }
   });
 }
 
 async function run(): Promise<void> {
-  // PR Title Check
-  const title = github.context.payload.pull_request?.title || '';
-  const {owner, repo} = github.context.repo;
+  const ONE_MINUTE_IN_MILLIS = 60_000;
+
   const bypassPrefix = process.env.CI ? core.getInput('BYPASS_PREFIX') : process.env.BYPASS_PREFIX;
   const gitBranch = process.env.CI ? core.getInput('GIT_BRANCH') : process.env.GIT_BRANCH;
-  const ONE_MINUTE_IN_MILLIS = 60_000;
+
+  const prTitle = github.context.payload.pull_request?.title || '';
+  const {owner, repo} = github.context.repo;
 
   // Authentication
   // https://github.com/actions/toolkit/tree/main/packages/github#usage
@@ -64,16 +79,16 @@ async function run(): Promise<void> {
   const octokit = github.getOctokit(`${githubToken}`);
 
   try {
-    if (!title && process.env.CI) {
+    if (!prTitle && process.env.CI) {
       console.log(
         `Skipping checks because action was not triggered in the context of a Pull Request on the CI environment.`
       );
       process.exit(0);
     }
 
-    const completedCheckSuites = await retry(
+    const latestRun = await retry(
       () => {
-        return fetchCompletedSuites(octokit, {
+        return fetchCompletedCheck(octokit, {
           gitBranch: `${gitBranch}`,
           owner,
           repo,
@@ -82,13 +97,6 @@ async function run(): Promise<void> {
       Infinity,
       ONE_MINUTE_IN_MILLIS
     );
-
-    if (completedCheckSuites.length === 0) {
-      const errorMessage = `There are no completed check suites on branch "${gitBranch}". Check suites are either pending or didn't run at all. You can read more about check suites here: https://docs.github.com/en/rest/guides/getting-started-with-the-checks-api#about-check-suites`;
-      throw new Error(errorMessage);
-    }
-
-    const latestRun = completedCheckSuites[completedCheckSuites.length - 1];
 
     const commitUrl = `https://github.com/${owner}/${repo}/commit/${latestRun.head_sha}`;
 
@@ -101,7 +109,7 @@ async function run(): Promise<void> {
     );
 
     // Check if there is a broken branch and if the PR addresses this with a commit that can bypass the status check
-    const bypassMergeCheck = title.startsWith(bypassPrefix);
+    const bypassMergeCheck = prTitle.startsWith(bypassPrefix);
 
     if (latestRun.conclusion === 'failure' && !bypassMergeCheck) {
       const errorMessage = `CI status check on branch "${gitBranch}" failed with this commit from "${latestRun.head_commit.author?.name}": ${commitUrl}`;
